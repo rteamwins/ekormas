d008cf4df3fd254abe0d25e19d7bb38264748360
Patrick Joel <joekenpat@gmail.com>
2020-08-20 22:15:56 +0100

    1st commit

---
+++ app/Http/Controllers/CashoutController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Cashout;
+use Illuminate\Http\Request;
+
+class CashoutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Cashout  $cashout
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Cashout $cashout)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Cashout  $cashout
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Cashout $cashout)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Cashout  $cashout
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Cashout $cashout)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Cashout  $cashout
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Cashout $cashout)
+    {
+        //
+    }
+}
