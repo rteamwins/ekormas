e6d125a9205fb4b8e5a3a2267d158aca63ebbdf7
Patrick Joel <joekenpat@gmail.com>
2021-02-07 23:13:08 +0100

    added logged to cron job

--- app/Console/Commands/MarkTradeAsCompleted.php
+++ app/Console/Commands/MarkTradeAsCompleted.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Trade;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class MarkTradeAsCompleted extends Command
 {
@@ -39,20 +40,20 @@ class MarkTradeAsCompleted extends Command
   public function handle()
   {
     $counter = 1;
-    $this->line('Mark trade as completed job Started');
+    Log::info('Mark trade as completed job Started');
     $trades = Trade::with(['user:id,wallet'])->where('closing_at', '<=', now())->whereCompleted(false)->get();
     foreach ($trades as $trade) {
-      $this->comment(sprintf('Processing Data: %s ', $counter));
+      Log::info(sprintf('Processing Data: %s ', $counter));
       $trade->user->wallet += ($trade->amount + $trade->earning);
       $trade->user->trading_capital -= $trade->amount;
       $trade->user->update();
       $trade->completed = true;
       $trade->update();
-      $this->info(sprintf('Added $%s to User: %s', number_format($trade->amount + $trade->earning), $trade->user->id));
-      $this->info(sprintf('Marked Trade: %s as Completed', $trade->id));
+      Log::info(sprintf('Added $%s to User: %s', number_format($trade->amount + $trade->earning), $trade->user->id));
+      Log::info(sprintf('Marked Trade: %s as Completed', $trade->id));
       $counter++;
     }
-    $this->line('Mark trade as completed job Ended');
+    Log::info('Mark trade as completed job Ended');
     $counter = null;
     return 0;
   }
