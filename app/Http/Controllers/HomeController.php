16928bfde04ef5dae87e0768264cc919f82c6349
Patrick Joel <joekenpat@gmail.com>
2021-02-13 23:53:36 +0100

    display the candle stick

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -233,7 +233,7 @@ class HomeController extends Controller
   public function user_dashboard()
   {
     $profits = Profit::whereUserId(Auth()->user()->id)->where([['created_at', '>=', now()->subDay()], ['created_at', '<=', now()->format('Y-m-d H:i:s')]])->latest()->get();
-    // $candle_sticks = $this->market_candlestick_bar();
+    $candle_sticks = $this->market_candlestick_bar();
     $plabels = [];
     $pdata = [];
     $series = [];
@@ -242,24 +242,24 @@ class HomeController extends Controller
       $pdata[] = number_format($profit->amount, 3);
     }
 
-    // foreach ($candle_sticks as $candle_stick) {
-    //   $series[] = [
-    //     "x" => $candle_stick['date'] * 1000,
-    //     "y" => [
-    //       $candle_stick['open'],
-    //       $candle_stick['high'],
-    //       $candle_stick['low'],
-    //       $candle_stick['close']
-    //     ]
-    //   ];
-    // }
-    // $mdata = [
+    foreach ($candle_sticks as $candle_stick) {
+      $series[] = [
+        "x" => $candle_stick['date'] * 1000,
+        "y" => [
+          $candle_stick['open'],
+          $candle_stick['high'],
+          $candle_stick['low'],
+          $candle_stick['close']
+        ]
+      ];
+    }
+    $mdata = [
 
-    //   "series" => [[
-    //     "data" => $series,
-    //     // "backgroundColor" => '#00C853',
-    //   ]]
-    // ];
+      "series" => [[
+        "data" => $series,
+        // "backgroundColor" => '#00C853',
+      ]]
+    ];
     $pdata = [
       "series" => [[
         "name" => 'Trade Profits',
@@ -270,7 +270,7 @@ class HomeController extends Controller
     ];
     return view('user.dashboard', [
       'pdata' => $pdata,
-      // 'mdata' => $mdata,
+      'mdata' => $mdata,
       'role' => auth()->user()->role,
       'trade_roi' => Trade::whereUserId(auth()->user()->id)->where('completed', false)->first()->earning ?? 0,
       'today_funding' => Transaction::where([
